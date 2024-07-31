f923399d875980c51cf5f6e345d0144aad78af53
raju120 <ashfaqraju27@gmail.com>
2024-07-31 12:42:25 +0600

    doctor list working perfectly

--- client/src/Pages/Modal/Modal.jsx
+++ client/src/Pages/Modal/Modal.jsx
@@ -5,7 +5,7 @@ export default function Modal({ setShowModal, data }) {
     <>
       <input type="checkbox" id="my_modal" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box relative h-auto w-[600px]">
+        <div className="modal-box w-11/12 max-w-3xl">
           <label htmlFor="my_modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
 
           {data?.doctors?.length > 0 ? (
@@ -13,32 +13,39 @@ export default function Modal({ setShowModal, data }) {
             data?.doctors?.map((doctor) => (
               <div key={doctor?.id}>
 
-                {/* {console.log("details",doctor)}
-                {console.log("name",doctor?.docname)} */}
+                 {console.log("details",doctor)}
+                {/*{console.log("name",doctor?.docname)} */}
                 {/* <h1 className="text-black">{doctor?.docname}</h1> */}
 
                 <div className="card bg-sky-700 text-neutral-content w-full top-6 mb-2">
                   <div className="card-body items-center text-center">
                     
-                    <div className='flex justify-between'>
+                    <div className='flex justify-center mr-auto'>
                       
-
-                        <div className="avatar">
-                          <div className="lg:w-36 w-12 rounded">
-                            <img src={doctor?.avatar} />
-                          </div>
+                      <div className="avatar">
+                        <div className="lg:w-48 w-12 rounded">
+                          <img src={doctor?.avatar} />
                         </div>
-
-                      
-                      <div>
-                        <h2 className="card-title mr-2">{doctor?.docname}</h2>
                       </div>
+                      
+                      <div className='ml-12'>
+                        <h2 className="font-bold text-3xl text-black mb-1 text-center">{doctor?.docname}</h2>
+                        <div className='text-start mt-2'>
+                          <p>Gender: <span className='text-sky-200 font-semibold'>{doctor?.gender}</span></p>
+                          <p>BMDC-Code: <span className='text-sky-200 font-semibold'>{doctor?.bmdc}</span></p>
+                          <p>Department Name: <span className='text-sky-200 font-semibold'>{doctor?.depname}</span></p>
+                          <p>Designation: <span className='text-sky-200 font-semibold'>{doctor?.designation}</span></p>
+                          <p>Qualification: <span className='text-sky-200 font-semibold'>{doctor?.qualification}</span></p>
+                          <p>Availabale Day: <span className='text-sky-200 font-semibold'>Today</span></p>
+                        </div>
+                      </div>
+
                     </div>
 
-                    <div className="card-actions justify-end">
-                      <button className="btn btn-primary">Accept</button>
-                      <button className="btn btn-ghost">Deny</button>
-                    </div>
+                    <div className="card-actions justify-end mt-2">
+                      <h1 className='card-title'>Address: <span className='text-black'>{doctor?.address}</span></h1>
+                    </div> 
+
                   </div>
                 </div>
 
@@ -48,6 +55,10 @@ export default function Modal({ setShowModal, data }) {
           ) : (
             <p>No doctors found.</p>
           )}
+
+          <div className="card-actions justify-end mt-5 ">
+            <label htmlFor="my_modal" className="btn btn-warning text-right">Close!</label>
+          </div>
         </div>
       </div>
     </>
