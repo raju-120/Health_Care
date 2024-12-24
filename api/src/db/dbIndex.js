212e6ad1b0ce6f2a41796fbf9287bca7f0e71555
raju-120 <ashfaqraju27@gmail.com>
2024-12-24 12:40:54 +0600

    db function added

--- api/src/db/dbIndex.js
+++ api/src/db/dbIndex.js
@@ -4,11 +4,16 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        console.log('MONGODB_URL-Index:', process.env.MONGODB_URL); // Debug log
+        console.log('MONGODB_URL:', process.env.MONGODB_URL); // Log the URL
+        if (!process.env.MONGODB_URL) {
+            throw new Error('MONGODB_URL is not defined');
+        }
+
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
+
         console.log(`MONGODB Connected! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MONGODB connection failed", error);
